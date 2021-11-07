c2f56b2b6c33ae51359a255e8706829db6caa668
sme777 <samson.petrosyan8@gmail.com>
2021-11-07 01:05:05 -0700

    add edge class for staple sequence generation

---
+++ app/javascript/packs/edge.js
@@ -0,0 +1,65 @@
+class Edge {
+    
+    // sequence is the portion of the scaffold from which strands will be generated
+    // length is the the length of the edge
+    // side is the side of the cube which is needed for B-type Refractions
+    constructor(sequence, length, side) {
+        this.sequence = sequence
+        this.strandLength = length
+        this.side = side
+        this.reflection = this.computeReflection()
+        this.refraction = this.computeRefraction()
+        this.extensions = this.computeExtensions()
+    }
+
+    computeReflection() {
+        const index = Math.floor(this.strandLength / 3) 
+        let subsequence
+        if (index > 25) {
+            subsequence = this.sequence.slice(-25) 
+            this.sequence = this.sequence.slice(0, -25)
+        } else if (index < 18) {
+            subsequence = this.sequence.slice(-18) 
+            this.sequence = this.sequence.slice(0, -18)
+        } else {
+            subsequence = this.sequence.slice(-index) 
+            this.sequence = this.sequence.slice(0, -index)
+        }
+        return subsequence
+    }
+
+    computeRefraction() {
+        const endIndex = Math.floor(this.strandLength / 3) 
+        const startIndex = Math.floor(2 * this.strandLength / 3)
+        let subsequence
+        if (endIndex > 25) {
+            subsequence = this.sequence.slice(0, 25)
+            this.sequence = this.sequence.slice(25)
+        } else if (startIndex < 18) {
+            subsequence = this.sequence.slice(0, startIndex)
+            this.sequence = this.sequence.slice(startIndex)
+        } else {
+            subsequence = this.sequence.slice(0, endIndex) 
+            this.sequence = this.sequence.slice(endIndex)
+        }
+        return subsequence
+    }
+
+    computeExtensions() {
+        const extensionLength = this.strandLength.length
+        const numOfStrands = Math.floor(extensionLength, 24)
+        let stapleArr = []
+        let subsequence
+        for (let i = 0; i < numOfStrands; i++) {
+            if (i == numOfStrands - 1) {
+                stapleArr.push(this.sequence)
+            } else {
+                subsequence = this.sequence.slice(0, 24)
+                this.sequence = this.sequence.slice(24)
+                stapleArr.push(subsequence)
+            }
+        }
+        return stapleArr
+    }
+
+}
