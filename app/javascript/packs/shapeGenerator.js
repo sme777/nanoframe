f0faa7e77b87e983dcfba54f98a171cba1afb9ba
sme777 <samson.petrosyan8@gmail.com>
2021-12-26 16:14:43 +0400

    refactor shapeGenerator

--- app/javascript/packs/shapeGenerator.js
+++ app/javascript/packs/shapeGenerator.js
@@ -1,44 +1,13 @@
-$("#step_size_input").on("input", function(evt) {
-    let self = $(this)
-    self.val(self.val().replace(/[^0-9\.]/g, ''))
-    if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57)) 
-    {
-      evt.preventDefault()
-    } 
-})
-
-$("#loopout_length_input").on("input", function(evt) {
-    let self = $(this)
-    self.val(self.val().replace(/[^0-9\.]/g, ''))
-    if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57)) 
-    {
-        evt.preventDefault()
-    } 
-})
-
-$("#max_input").on("input", function(evt) {
-    let self = $(this)
-    self.val(self.val().replace(/[^0-9\.]/g, ''))
-    if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57)) 
-    {
-      evt.preventDefault()
-    } 
-})
-
-$("#min_input").on("input", function(evt) {
-    let self = $(this)
-    self.val(self.val().replace(/[^0-9\.]/g, ''))
-    if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57)) 
-    {
-        evt.preventDefault()
-    } 
-})
-
-$("#scaff_length_input").on("input", function(evt) {
-    let self = $(this)
-    self.val(self.val().replace(/[^0-9\.]/g, ''))
-    if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57)) 
-    {
-      evt.preventDefault()
-    } 
+$(document).ready(function() {
+    const fields = ["step_size", "loopout_length", "max", "min", "scaff_length"]
+    for (let i in fields) {
+        $(`#${fields[i]}_input`).on("input", function(evt) {
+            let self = $(this)
+            self.val(self.val().replace(/[^0-9\.]/g, ''))
+            if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57)) 
+            {
+              evt.preventDefault()
+            } 
+        })
+    }
 })
