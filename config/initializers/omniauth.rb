b7fc2b91831b08fc6804ed1c3fac50f740c78ab6
sme777 <samson.petrosyan8@gmail.com>
2022-02-22 13:37:40 -0800

    remove omniauth builder excess lines

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -38,37 +38,6 @@ end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
     setup_omniauth_mocks unless Rails.env.production?
-    byebug
     provider :github, Rails.application.credentials[:GITHUB_KEY], Rails.application.credentials[:GITHUB_SECRET]
     provider :google_oauth2, Rails.application.credentials[:GOOGLE_CLIENT_ID], Rails.application.credentials[:GOOGLE_CLIENT_SECRET]
-#   unless Rails.env.production?
-#     OmniAuth.config.test_mode = true
-#     user = YAML.safe_load(File.read("#{Rails.root}/db/github_mock_login.yml"))[Rails.env]
-#     OmniAuth.config.add_mock(:github,
-#                              {
-#                                uid: '12345',
-#                                info: {
-#                                  name: user['name'],
-#                                  nickname: user['github_uid'],
-#                                  email: user['email'],
-#                                  type_user: user['user_type']
-#                                }
-#                              })
-
-#   end
-
-#   provider :google_oauth2, Rails.application.credentials[:GOOGLE_CLIENT_ID],
-#            Rails.application.credentials[:GOOGLE_CLIENT_SECRET]
-#   unless Rails.env.production?
-#     OmniAuth.config.test_mode = true
-#     user = YAML.safe_load(File.read("#{Rails.root}/db/google_mock_login.yml"))[Rails.env]
-#     OmniAuth.config.add_mock(:google_oauth2,
-#                              {
-#                                uid: '54321',
-#                                info: {
-#                                  name: user['name'],
-#                                  email: user['email']
-#                                }
-#                              })
-#   end
 end
