3c61d0330dc2680ce08a79f79866d563e980a4d1
sme777 <samson.petrosyan8@gmail.com>
2022-04-10 11:22:51 -0700

    add cbc initializer

---
+++ config/initializers/cbc_starter.rb
@@ -0,0 +1,5 @@
+if Rails.env.production? || Rails.env.staging?
+    ENV['GDAL_DATA'] = ".apt/usr/share/gdal/2.1"
+    # heroku-buildpack-apt does not run postinstall scripts
+    ENV['LD_LIBRARY_PATH'] = ".apt/usr/lib/libblas:.apt/usr/lib/lapack:#{ENV['LD_LIBRARY_PATH']}"
+end
