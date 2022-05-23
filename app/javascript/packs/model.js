0503e5772d22bc2db63404391ae2367acb635823
sme777 <samson.petrosyan8@gmail.com>
2022-05-23 10:14:31 -0700

    update shape perspective in generator index view

--- app/javascript/packs/model.js
+++ app/javascript/packs/model.js
@@ -13,17 +13,23 @@ const renderer = new THREE.WebGLRenderer({
   canvas,
 });
 const fov = 25;
-const aspect = 4;
 const near = 0.1;
 const far = 10000;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+const canvasContainer = document.getElementById("canvas-container");
+let canvasContainerWidth = canvasContainer.offsetWidth;
+let canvasContainerHeight = canvasContainer.offsetHeight;
+const camera = new THREE.PerspectiveCamera(
+  fov, 
+  canvasContainerWidth / canvasContainerHeight, 
+  near, 
+  far
+);
 const controls = new OrbitControls(camera, renderer.domElement);
 let sequence;
 controls.enableDamping = true;
 const gui = new dat.GUI({
   autoPlace: false,
 });
-// document.querySelector(".datGUI").append(gui.domElement);
 
 camera.position.z = 240;
 const scene = new THREE.Scene();
@@ -35,454 +41,26 @@ let meshData;
 let isGUISet = false;
 let guiElements = [];
 let dna = new DNA(7249);
-const geometry = new THREE.BufferGeometry().setFromPoints(dna.positions);
-const material = new THREE.LineBasicMaterial({
-  color: 0xff0000,
-});
-const curveObject = new THREE.Line(geometry, material);
+// const geometry = new THREE.BufferGeometry().setFromPoints(dna.positions);
+// const material = new THREE.LineBasicMaterial({
+//   color: 0xff0000,
+// });
+// const curveObject = new THREE.Line(geometry, material);
 // const axesHelper = new THREE.AxesHelper(15);
 // scene.add(axesHelper);
 
-const canvasContainer = document.getElementById("canvas-container");
+
 renderer.setPixelRatio(window.devicePixelRatio);
-let canvasContainerWidth = canvasContainer.offsetWidth;
-let canvasContainerHeight = canvasContainer.offsetHeight;
 renderer.setSize(canvasContainerWidth, canvasContainerHeight);
 
 mesh = Maker.makeCube();
-// console.log(mesh)
-// meshData = Data.cubeData;
-// scene.add(mesh);
-// meshData = Data.torusKnotData;
 
 
-// shape.addEventListener("click", function () {
-//   chosenShape = shape.value;
-//   let setGuiElement = (func, obj) => {
-//     guiElements.push(
-//       gui
-//         .add(meshData, obj["property"], obj["min"], obj["max"])
-//         .step(obj["step"])
-//         .name(obj["title"])
-//         .setValue(obj["set"])
-//         .onChange(() => {
-//           func(mesh);
-//         })
-//     );
-//   };
-//   if (mesh != null) {
-//     mesh.geometry.dispose();
-//     mesh.material.dispose();
-//     scene.remove(mesh);
-//   }
-//   if (chosenShape == 0) {
-//     mesh = Maker.makeDefault();
-//   } else if (chosenShape == 1) {
-//     clearGUIElemens();
-//     mesh = Maker.makeCube();
-//     meshData = Data.cubeData;
+if (mesh != undefined) {
+  scene.add(mesh);
+  requestAnimationFrame(render);
+}
 
-//     // setGuiElement(Maker.makeCube, {
-//     //   shape: "Cube",
-//     //   min: 0,
-//     //   max: 240,
-//     //   set: 30,
-//     //   title: "Width",
-//     //   property: "width",
-//     //   step: 0.1,
-//     // });
-//     // setGuiElement(Maker.makeCube, {
-//     //   shape: "Cube",
-//     //   min: 0,
-//     //   max: 240,
-//     //   set: 30,
-//     //   title: "Height",
-//     //   property: "height",
-//     //   step: 0.1,
-//     // });
-//     // setGuiElement(Maker.makeCube, {
-//     //   shape: "Cube",
-//     //   min: 0,
-//     //   max: 240,
-//     //   set: 30,
-//     //   title: "Depth",
-//     //   property: "depth",
-//     //   step: 0.1,
-//     // });
-//     // setGuiElement(Maker.makeCube, {
-//     //   shape: "Cube",
-//     //   min: 1,
-//     //   max: 15,
-//     //   set: 2,
-//     //   title: "Width Stripes",
-//     //   property: "widthSegments",
-//     //   step: 1,
-//     // });
-//     // setGuiElement(Maker.makeCube, {
-//     //   shape: "Cube",
-//     //   min: 1,
-//     //   max: 15,
-//     //   set: 2,
-//     //   title: "Height Stripes",
-//     //   property: "heightSegments",
-//     //   step: 1,
-//     // });
-//     // setGuiElement(Maker.makeCube, {
-//     //   shape: "Cube",
-//     //   min: 1,
-//     //   max: 15,
-//     //   set: 2,
-//     //   title: "Depth Stripes",
-//     //   property: "depthSegments",
-//     //   step: 1,
-//     // });
-//   } else if (chosenShape == 2) {
-//     clearGUIElemens();
-//     mesh = Maker.makeSphere();
-//     meshData = Data.sphereData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radius", 0, 50)
-//         .name("Radius")
-//         .setValue(22)
-//         .onChange(() => {
-//           Maker.makeSphere(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "widthSegments", 0, 25)
-//         .step(1)
-//         .name("Width Segment")
-//         .setValue(10)
-//         .onChange(() => {
-//           Maker.makeSphere(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "heightSegments", 0, 25)
-//         .step(1)
-//         .name("Height Segment")
-//         .setValue(8)
-//         .onChange(() => {
-//           Maker.makeSphere(mesh);
-//         })
-//     );
-//   } else if (chosenShape == 3) {
-//     clearGUIElemens();
-//     mesh = Maker.makeCylinder();
-//     meshData = Data.cylinderData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "height", 0, 50)
-//         .name("Height")
-//         .setValue(33)
-//         .onChange(() => {
-//           Maker.makeCylinder(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radialSegments", 2, 25)
-//         .name("Radial Segment")
-//         .setValue(12)
-//         .onChange(() => {
-//           Maker.makeCylinder(mesh);
-//         })
-//     );
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radiusTop", 0, 30)
-//         .name("Radius Top")
-//         .setValue(18)
-//         .onChange(() => {
-//           Maker.makeCylinder(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radiusBottom", 0, 30)
-//         .name("Radius Bottom")
-//         .setValue(10)
-//         .onChange(() => {
-//           Maker.makeCylinder(mesh);
-//         })
-//     );
-//   } else if (chosenShape == 4) {
-//     clearGUIElemens();
-//     mesh = Maker.makeCone();
-//     meshData = Data.coneData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "height", 0, 50)
-//         .name("Height")
-//         .setValue(33)
-//         .onChange(() => {
-//           Maker.makeCone(mesh);
-//         })
-//     );
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radius", 0, 40)
-//         .name("Radius")
-//         .setValue(20)
-//         .onChange(() => {
-//           Maker.makeCone(mesh);
-//         })
-//     );
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radialSegments", 2, 40)
-//         .step(1)
-//         .name("Radial Segment")
-//         .setValue(20)
-//         .onChange(() => {
-//           Maker.makeCone(mesh);
-//         })
-//     );
-//   } else if (chosenShape == 5) {
-//     clearGUIElemens();
-//     mesh = Maker.makePolyhedron();
-//     meshData = Data.polyhedronData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radius", 0, 40)
-//         .name("Radius")
-//         .setValue(22)
-//         .onChange(() => {
-//           Maker.makePolyhedron(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "detail", 0, 10)
-//         .name("Detail")
-//         .step(1)
-//         .setValue(0)
-//         .onChange(() => {
-//           Maker.makePolyhedron(mesh);
-//         })
-//     );
-//   } else if (chosenShape == 6) {
-//     clearGUIElemens();
-//     mesh = Maker.makeTetrahedron();
-//     meshData = Data.tetrahedronData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radius", 0, 40)
-//         .name("Radius")
-//         .setValue(22)
-//         .onChange(() => {
-//           Maker.makeTetrahedron(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "detail", 0, 10)
-//         .name("Detail")
-//         .step(1)
-//         .setValue(0)
-//         .onChange(() => {
-//           Maker.makeTetrahedron(mesh);
-//         })
-//     );
-//   } else if (chosenShape == 7) {
-//     clearGUIElemens();
-//     mesh = Maker.makeOctahedron();
-//     meshData = Data.octahedronData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radius", 0, 40)
-//         .name("Radius")
-//         .setValue(22)
-//         .onChange(() => {
-//           Maker.makeOctahedron(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "detail", 0, 10)
-//         .name("Detail")
-//         .step(1)
-//         .setValue(0)
-//         .onChange(() => {
-//           Maker.makeOctahedron(mesh);
-//         })
-//     );
-//   } else if (chosenShape == 8) {
-//     clearGUIElemens();
-//     mesh = Maker.makeIcosahedron();
-//     meshData = Data.icosahedronData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radius", 0, 40)
-//         .name("Radius")
-//         .setValue(22)
-//         .onChange(() => {
-//           Maker.makeIcosahedron(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "detail", 0, 10)
-//         .name("Detail")
-//         .step(1)
-//         .setValue(0)
-//         .onChange(() => {
-//           Maker.makeIcosahedron(mesh);
-//         })
-//     );
-//   } else if (chosenShape == 9) {
-//     clearGUIElemens();
-//     mesh = Maker.makeDodecahedron();
-//     meshData = Data.dodecahedronData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radius", 0, 40)
-//         .name("Radius")
-//         .setValue(22)
-//         .onChange(() => {
-//           Maker.makeDodecahedron(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "detail", 0, 10)
-//         .name("Detail")
-//         .step(1)
-//         .setValue(0)
-//         .onChange(() => {
-//           Maker.makeDodecahedron(mesh);
-//         })
-//     );
-//   } else if (chosenShape == 10) {
-//     clearGUIElemens();
-//     mesh = Maker.makeTorus();
-//     meshData = Data.torusData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radius", 0, 40)
-//         .name("Radius")
-//         .setValue(22)
-//         .onChange(() => {
-//           Maker.makeTorus(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radialSegments", 2, 30)
-//         .name("Radial Segment")
-//         .step(1)
-//         .setValue(8)
-//         .onChange(() => {
-//           Maker.makeTorus(mesh);
-//         })
-//     );
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "tubeRadius", 0, 15)
-//         .name("Tube Radius")
-//         .setValue(2)
-//         .onChange(() => {
-//           Maker.makeTorus(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "tubularSegments", 2, 50)
-//         .name("Tubular Segment")
-//         .step(1)
-//         .setValue(24)
-//         .onChange(() => {
-//           Maker.makeTorus(mesh);
-//         })
-//     );
-//   } else {
-//     clearGUIElemens();
-//     mesh = Maker.makeTorusKnot();
-//     meshData = Data.torusKnotData;
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radius", 0, 40)
-//         .name("Radius")
-//         .setValue(15)
-//         .onChange(() => {
-//           Maker.makeTorusKnot(mesh);
-//         })
-//     );
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "radialSegments", 2, 30)
-//         .name("Radial Segment")
-//         .step(1)
-//         .setValue(8)
-//         .onChange(() => {
-//           Maker.makeTorusKnot(mesh);
-//         })
-//     );
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "tubeRadius", 0, 20)
-//         .name("Tube Radius")
-//         .setValue(4)
-//         .onChange(() => {
-//           Maker.makeTorusKnot(mesh);
-//         })
-//     );
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "tubularSegments", 2, 100)
-//         .name("Tubular Segment")
-//         .step(1)
-//         .setValue(64)
-//         .onChange(() => {
-//           Maker.makeTorusKnot(mesh);
-//         })
-//     );
-
-//     guiElements.push(
-//       gui
-//         .add(meshData, "p", 0.1, 10)
-//         .name("P")
-//         .setValue(2)
-//         .onChange(() => {
-//           Maker.makeTorusKnot(mesh);
-//         })
-//     );
-//     guiElements.push(
-//       gui
-//         .add(meshData, "q", 0.1, 10)
-//         .name("Q")
-//         .setValue(3)
-//         .onChange(() => {
-//           Maker.makeTorusKnot(mesh);
-//         })
-//     );
-//   }
-
-  if (mesh != undefined) {
-    scene.add(mesh);
-    requestAnimationFrame(render);
-  }
-// });
 requestAnimationFrame(render);
 
 function clearGUIElemens() {
@@ -504,9 +82,6 @@ function resizeRendererToDisplaySize(renderer) {
 }
 
 function render(time) {
-  // if (chosenShape == undefined) {
-  //   return;
-  // }
 
   time *= 0.001;
 
@@ -518,9 +93,6 @@ function render(time) {
 
   const speed = 0.2;
   const rot = time * speed;
-  // mesh.rotation.x = rot
-  curveObject.rotation.z = rot / 10;
-  curveObject.rotation.x = rot / 10;
   mesh.rotation.y = rot;
 
   renderer.render(scene, camera);
