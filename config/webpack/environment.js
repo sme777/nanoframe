d51dc8bd2badb9599ee1a54b226d2eaff6778854
sme777 <samson.petrosyan8@gmail.com>
2022-06-12 11:49:32 -0700

    update bootstrap config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,11 @@
 const { environment } = require("@rails/webpacker");
 const erb = require("./loaders/erb");
-
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default']
+  }))
 environment.loaders.prepend("erb", erb);
 module.exports = environment;
