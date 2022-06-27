048415988eae8d4e8643edf1b83a340e925322d9
sme777 <samson.petrosyan8@gmail.com>
2022-06-27 01:04:03 -0700

    new navbar design addition

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ import "channels";
 
 import 'bootstrap/dist/js/bootstrap'
 import "bootstrap/dist/css/bootstrap";
+import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start();
 Turbolinks.start();
